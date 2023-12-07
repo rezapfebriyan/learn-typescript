576a9011bd3604b0e5f9a5e52f850a2064c50ebd
rezapfebriyan <rezafebriyan00@gmail.com>
2023-12-07 11:26:15 +0700

    feat: API update product

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -5,8 +5,28 @@ import IController from "./IController"
 import Product from "../models/Product"
 
 class ProductController implements IController {
-    getAll(req: Request, res: Response): Response {
-        return res.status(200).json({result: req.body.data})
+    getAll = async (req: Request, res: Response): Promise<Response> => {
+        const { id }  = req.app.locals.user
+        try {
+            const data = await Product.findAll({
+                attributes:[ 'id', 'name', 'price' ],
+                where: {
+                    userId: id // req from data middleware
+                },
+                include:[{ 
+                    model: User,
+                    attributes:['email', 'name']
+                }] // ada relasi Product ke User
+            })
+            return res.status(200)
+            .json({ code : 200, data })
+
+    
+        } catch (error: any) {
+            return res.status(500)
+                .json({ code : 500,
+                message: error.message })
+        }
     }
     store = async (req: Request, res: Response): Promise<Response> => {
         const { id }  = req.app.locals.user
@@ -31,10 +51,40 @@ class ProductController implements IController {
                     message: error.message
                 });
         }
-        return res.status(200).json({result: req.body.data})
     }
-    update(req: Request, res: Response): Response {
-        return res.status(200).json({result: req.body.data})
+    update = async (req: Request, res: Response): Promise<Response> => {
+        try {
+            const product = await Product.findOne({
+                where: { id: req.params.id }
+            })
+    
+            if (!product) return res.status(404).json({
+                code : 404,
+                message: "Product not found" 
+            })
+    
+            const { id: userId }  = req.app.locals.user
+            const { name, price } = req.body
+    
+            const data = await Product.update(
+                { name, price },
+                {
+                    where: { id:  product.id, userId }
+                }
+            )
+    
+            return res.status(200).json({ 
+                code : 200,
+                message: "Product has been updated"
+            })
+        } catch (error: any) {
+            return res.status(500)
+                .json({
+                    code: 500,
+                    message: error.message
+                });
+        }
+
     }
     delete(req: Request, res: Response): Response {
         return res.status(200).json({result: req.body.data})
